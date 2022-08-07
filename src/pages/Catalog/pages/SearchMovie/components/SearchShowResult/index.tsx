f1824b9aa55963989fbf8941768145ac90b51a01
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2022-08-07 21:25:47 +0700

    [fix warning]

--- src/pages/Catalog/pages/SearchMovie/components/SearchShowResult/index.tsx
+++ src/pages/Catalog/pages/SearchMovie/components/SearchShowResult/index.tsx
@@ -5,7 +5,7 @@ import SearchShowEmpty from "./SearchShowEmpty";
 import SearchTVItem from "./SearchItem/SearchTvItem";
 import useSearchShowResultStyles from "./styles/SearchShowResult.styles";
 import SearchPeopleItem from "./SearchItem/SearchPeopleItem";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface ItemProps {
   id: number;
@@ -26,22 +26,26 @@ const Item = (data: ItemProps) => {
 
 const SearchShowResult = () => {
   const styles = useSearchShowResultStyles();
+  const navigator = useNavigate();
   const datas = useAppSelector(selectAllSearchs);
   if (datas.length === 0) return <SearchShowEmpty />;
   return (
     <div className={styles.searchShowResult}>
       {datas.map((data) => (
-        <Link
+        <div
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            navigator(`/${data.media_type}/${data.id}`);
+          }}
           key={`${data.media_type}-${data.id}`}
           className='normal'
-          to={`/${data.media_type}/${data.id}`}
         >
           <Item
             id={data.id}
             className={styles.searchShowResultItem}
             mediaType={data.media_type}
           />
-        </Link>
+        </div>
       ))}
     </div>
   );
